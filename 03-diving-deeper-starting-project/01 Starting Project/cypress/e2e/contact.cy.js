4de0f26d18a25a9a03cc41a5150f38b288eb4768
summerine <indri.pr.26@gmail.com>
2023-11-27 22:30:13 +0700

    screenshot, alias, and parent

--- 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/contact.cy.js
+++ 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/contact.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="Cypress" />
 
+//happy or positive test cases 
+//1. fill in message, name and email 
+//2. click on submit button 
+//expected result: "Send Message" button should be changed to "Sending..." 
 describe('contact form', () => {
     it('should submit the form', () => {
         cy.visit('http://localhost:5173/about')
@@ -23,12 +27,37 @@ describe('contact form', () => {
         cy.get('@submitBtn').should('have.attr', "disabled") //to check whether the disabled attribute is set 
     });
 
-    it.only('should validate the form input', () => {
+    // negative test cases 
+    it('should validate the form input', () => {
         cy.visit('http://localhost:5173/about')
         cy.get('[data-cy="contact-btn-submit"]').click()
         cy.get('[data-cy="contact-btn-submit"]').then(el => {
             expect(el).to.not.have.attr('disabled')
-            expect(el.text()).to.not.have.equal('Sending...')
+            expect(el.text()).to.not.equal('Sending...')
         })
+        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message')
+        cy.get('[data-cy="contact-input-message"]').as('msgInput') //use alias 
+        cy.screenshot()
+        cy.get('@msgInput').focus().blur() //focus on a dom element then blur a focused element
+        cy.get('@msgInput')
+        .parent() //going one level up 
+        .should('have.attr', 'class').and('match', /invalid/)
+
+        cy.get('[data-cy="contact-input-name"]').as('inputName') // use alias 
+        cy.screenshot() //do screenshot on the certain period of test 
+        cy.get('@inputName').focus().blur() //focus on a dom element then blur a focused element
+        cy.get('@inputName')
+        .parent()
+        .should('have.attr', 'class').and('match', /invalid/) 
+        /*the assertion will pass if class attribute on this parent, 
+        that contains the word 'invalid' (must include the word invalid) */
+
+        cy.get('[data-cy="contact-input-email"]').as('inputEmail') //use alias 
+        cy.screenshot()
+        cy.get('@inputEmail').focus().blur() //focus on a dom element then blur a focused element
+        cy.get('@inputEmail')
+        .parent()
+        .should('have.attr', 'class').and('match', /invalid/)
+
     })
 })
